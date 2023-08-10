b8b4633892db586c3793c90dd9522431090e4846
yandaki <boayves@gmail.com>
2023-08-10 14:57:13 +0000

    configuration app, implementation template file

--- jokesapp/views.py
+++ jokesapp/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+def Index(request):
+    return HttpResponse("Index Page")

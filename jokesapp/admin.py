1a6c51ba0a3bb420a445ea77afa6c6b40cb937ca
yandaki <boayves@gmail.com>
2023-08-10 17:43:25 +0000

    Implementation admin page

--- jokesapp/admin.py
+++ jokesapp/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Joke,Category
+
+class JokeLine(admin.TabularInline):
+    model = Joke
+    extra = 3
+
+class CategoryAdmin(admin.ModelAdmin):
+    
+    fieldsets = (
+        (None, {
+            "fields": (
+                ["libelle"]
+            ),
+        }),
+    )
+    
+    inlines = [JokeLine]
+    
 
 # Register your models here.
+admin.site.register(Category , CategoryAdmin)
+admin.site.register(Joke)
+# hpelitebook840
--- jokesapp/models.py
+++ jokesapp/models.py
@@ -8,14 +8,20 @@ class Category(models.Model):
     libelle = models.CharField(max_length=180)
     view = models.BooleanField(default=True)
     
+    def __str__(self):
+        return self.libelle
+    
 
 class Joke(models.Model):
+    
     category = models.ForeignKey(Category , on_delete=models.CASCADE)
     question = models.CharField(max_length=300)
     answer = models.CharField(max_length=150)
     date_pub = models.DateTimeField("Date pub")
     view = models.BooleanField(default=True)
     
+    def __str__(self) -> str:
+        return self.question
     
     def is_recent(self):
         time = timezone.now()

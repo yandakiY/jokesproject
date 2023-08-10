a805ed59a47d4a751998bab876b42de6c9e2b6b5
yandaki <boayves@gmail.com>
2023-08-10 16:04:27 +0000

    Implementation model Category and Joke

--- jokesapp/models.py
+++ jokesapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
+class Category(models.Model):
+    
+    libelle = models.CharField(max_length=180)
+    view = models.BooleanField(default=True)
+    
+
+class Joke(models.Model):
+    category = models.ForeignKey(Category , on_delete=models.CASCADE)
+    question = models.CharField(max_length=300)
+    answer = models.CharField(max_length=150)
+    date_pub = models.DateTimeField("Date pub")
+    view = models.BooleanField(default=True)
+    
+    
+    def is_recent(self):
+        time = timezone.now()
+        return time - datetime.timedelta(days=1) <= self.date_pub <= time
--- jokesapp/views.py
+++ jokesapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 def Index(request):
-    return HttpResponse("Index Page")
+    return render(request , "jokesapp/index.html",{"name":"Boa Yandaki Yves Michel"})

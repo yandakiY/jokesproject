a68000a517e6871f9848d86da9ba5e2bf477b412
yandaki <boayves@gmail.com>
2023-08-25 01:32:20 +0000

    implementation update and delete

--- jokesapp/urls.py
+++ jokesapp/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = "jokesapp"
 
 urlpatterns = [
-    path('' , views.Index , name="Index")
+    path('' , views.Index.as_view() , name="Index"),
+    path('create', views.create , name="create"),
+    path('post_create' , views.post_create , name="post_create"),
+    path('<int:joke_id>/update' , views.update , name="update"),
+    path('<int:joke_id>/post_update' , views.post_update , name="post_update"),
+    path('<int:joke_id>/delete' , views.delete , name="delete"),
 ]
--- jokesapp/views.py
+++ jokesapp/views.py
@@ -1,5 +1,79 @@
-from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect, get_list_or_404 , get_object_or_404
+from .models import Joke , Category
+from django.views import generic
+from django.urls import reverse
+from django.utils import timezone
 
 # Create your views here.
-def Index(request):
-    return render(request , "jokesapp/index.html",{"name":"Boa Yandaki Yves Michel"})
+class Index(generic.ListView):
+    
+    template_name = "jokesapp/index.html"
+    context_object_name = "jokes"
+    
+    def get_queryset(self):
+        return Joke.objects.all().order_by('-date_pub');
+    
+
+# views create jokes page 
+def create(request):
+    # we need to send list of category in this page
+    list_cat = Category.objects.all();
+    print(list_cat)
+    return render(request , "jokesapp/create.html" , {"categories":list_cat})
+
+# post method for create jokes page 
+def post_create(request):
+    # print(request.POST.get("category"))
+    # print(request.POST.get("question"))
+    # print(request.POST.get("answer"))
+    # print(timezone.now())
+    
+    # get category eq to cat
+    cat = Category.objects.get(id = request.POST.get("category"))
+    
+    # jokes , created = Joke.objects.get_or_create(category=request.POST.get("category") , question=request.POST.get("question") , answer=request.POST.get("answer") , date_pub=timezone.now())
+    j = Joke(category=cat , question=request.POST.get("question") , answer=request.POST.get("answer") , date_pub=timezone.now())
+    j.save()
+    
+    return HttpResponseRedirect(reverse("jokesapp:Index" , args=()))
+
+
+def update(request , joke_id):
+    # get joke correspond to id 
+    j = Joke.objects.get(id = joke_id)
+    print(j.category.id)
+    
+    list_cat = Category.objects.all();
+    print(list_cat)
+    return render(request , "jokesapp/update.html" , {"categories":list_cat , "joke":j})
+
+def post_update(request , joke_id):
+    # get joke which correspond to id
+    j = Joke.objects.get(id = joke_id)
+    
+    # value of category
+    cat = Category.objects.get(id = request.POST.get("category"))
+    
+    # get joke
+    j = get_object_or_404(Joke , id=joke_id)
+    
+    j.category = cat
+    j.question = request.POST.get("question")
+    j.answer = request.POST.get("answer")
+    
+    # save all changes
+    j.save()
+    
+    return HttpResponseRedirect(reverse("jokesapp:Index" , args=()))
+
+
+def delete(request , joke_id):
+    # get joke 
+    j = Joke.objects.get(id = joke_id)
+    # j = get_object_or_404(Joke , id=joke_id)
+    print(j.question)
+    j.delete()
+    # dont need to add save()
+    
+    # redirection
+    return HttpResponseRedirect(reverse("jokesapp:Index" , args=()))
